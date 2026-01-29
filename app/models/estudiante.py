69fa10fd41b86fa846786da667948f31e6bfda52
andersongeng <andersongranado28@gmail.com>
2026-01-28 22:59:27 -0400

    Create: Estudiante model

---
+++ app/models/estudiante.py
@@ -0,0 +1,10 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from app.extensions import db
+
+class Estudiante(db.Model):
+    __tablename__ = 'estudiante'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    nombre: Mapped[str] = mapped_column(nullable=False)
+    apellido: Mapped[str] = mapped_column(nullable=False)
+    cedula: Mapped[int] = mapped_column(nullable=False, unique=True)
