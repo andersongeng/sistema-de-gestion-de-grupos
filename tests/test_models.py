432be22378939de822dffb78a9df1f6f4e229b40
andersongeng <andersongranado28@gmail.com>
2026-01-29 18:22:40 -0400

    Add: test_crear_inscripcion method

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,11 +1,12 @@
 from app.extensions import db
+from app.models.estudiante import Estudiante
+from app.models.grupo import Grupo
 
 def test_crear_estudiante(app):
     """
     Prueba que un estudiante se guarda correctamente en la base de datos.
     El argumento 'app' viene de el fixture en conftest.py
     """
-    from app.models.estudiante import Estudiante
 
     # 1. Crear el registro
     nuevo_estudiante = Estudiante(
@@ -32,7 +33,6 @@ def test_crear_grupo(app):
     Prueba que un grupo se guarda correctamente en la base de datos.
     El argumento 'app' viene de el fixture en conftest.py
     """
-    from app.models.grupo import Grupo
 
     # 1. Crear el registro
     nuevo_grupo = Grupo(
@@ -51,3 +51,46 @@ def test_crear_grupo(app):
         assert grupo_db is not None
         assert grupo_db.numero == 1
         assert grupo_db.id is not None
+
+def test_crear_inscripcion(app):
+    """
+    Prueba que se puedan inscribir estudiantes en un grupo
+    y guardarse correctamente en la base de datos.
+    El argumento 'app' viene de el fixture en conftest.py
+    """
+    # 1. Crear los registros
+    estudiante_1 = Estudiante(
+        nombre="Anderson",
+        apellido="Granado",
+        cedula=1
+    )
+
+    estudiante_2 = Estudiante(
+        nombre="Fulano",
+        apellido="Fulanito",
+        cedula=2
+    )
+
+    nuevo_grupo = Grupo(
+        numero=1
+    )
+
+    # 2. Abrir el contexto de la base de datos para operar
+    with app.app_context():
+        db.session.add_all([estudiante_1, estudiante_2, nuevo_grupo])
+        db.session.commit()
+
+        # 3. Inscribir usando .append()
+
+        nuevo_grupo.estudiantes.append(estudiante_1)
+        nuevo_grupo.estudiantes.append(estudiante_2)
+        db.session.commit()
+
+        # 4. Verificacion de ida: Grupo tiene a los estudiantes
+        assert len(nuevo_grupo.estudiantes) == 2
+        assert estudiante_1 in nuevo_grupo.estudiantes
+        assert estudiante_2 in nuevo_grupo.estudiantes
+
+        # 5. Verificacion de vuelta: El estudiante sabe en que grupo esta
+        assert len(estudiante_1.grupos) == 1
+        assert nuevo_grupo in estudiante_1.grupos
