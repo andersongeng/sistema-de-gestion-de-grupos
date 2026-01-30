c87596aa80e6b313c53a7fa16ad93bd574e676ea
andersongeng <andersongranado28@gmail.com>
2026-01-29 22:35:13 -0400

    Clean: id column to use inherited id column

--- app/models/estudiante.py
+++ app/models/estudiante.py
@@ -4,7 +4,6 @@ from app.extensions import db
 class Estudiante(db.Model):
     __tablename__ = 'estudiantes'
 
-    id: Mapped[int] = mapped_column(primary_key=True)
     nombre: Mapped[str] = mapped_column(nullable=False)
     apellido: Mapped[str] = mapped_column(nullable=False)
     cedula: Mapped[int] = mapped_column(nullable=False, unique=True)
