9eb764713fdd63a0937fc140cb9f715d333da4e7
andersongeng <andersongranado28@gmail.com>
2026-01-29 22:08:19 -0400

    Add: abstract property and implement polymorphic factory method in BaseModel

--- app/extensions.py
+++ app/extensions.py
@@ -3,7 +3,17 @@ from sqlalchemy.orm import DeclarativeBase
 from flask_migrate import Migrate
 
 class Base(DeclarativeBase):
-    pass
+    __abstract__ = True
+
+    @classmethod
+    def create(cls, **kwargs):
+        """
+        Method to create any record.
+        """
+        nuevo_objeto = cls(**kwargs)
+        db.session.add(nuevo_objeto)
+        db.session.commit()
+        return nuevo_objeto
 
 db = SQLAlchemy(model_class=Base)
 migrate = Migrate()
